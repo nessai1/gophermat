a1854fea4bc7361d384aaf042d04b7dd7a70f001
nessai1 <werowkin@gmail.com>
2023-11-17 00:32:31 +0200

    Репозиторий пользователей под postgres

---
+++ internal/user/pgx-repository.go
@@ -0,0 +1,48 @@
+package user
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+type PGXRepository struct {
+	db *sql.DB
+}
+
+func (repository *PGXRepository) GetUserByLogin(login string) (*User, error) {
+	row := repository.db.QueryRow("SELECT login, password, balance FROM user WHERE login = $1", login)
+
+	if row.Err() != nil && errors.Is(row.Err(), sql.ErrNoRows) {
+		return nil, ErrUserNotFound
+	} else if row.Err() != nil {
+		return nil, fmt.Errorf("error while query for get user by login: %w", row.Err())
+	}
+
+	var user User
+
+	err := row.Scan(&user.Login, &user.password, &user.Balance)
+	if err != nil {
+		return nil, fmt.Errorf("error while scan row for get user by login: %w", err)
+	}
+
+	return &user, nil
+}
+
+func (repository *PGXRepository) CreateUser(user *User) error {
+	_, err := repository.db.Exec("INSERT INTO user(login, password, balance) VALUES ($1, $2, $3)", user.Login, user.password, user.Balance)
+
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) {
+			if pgErr.Code == "23505" {
+				return ErrLoginAlreadyExists
+			}
+		}
+
+		return fmt.Errorf("error while creating user: %w", err)
+	}
+
+	return nil
+}
